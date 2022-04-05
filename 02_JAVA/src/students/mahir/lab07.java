dc6518e43b6110b145927adbe3d1f8f6f157f86d
mahiralkan <mahir.alkan.100@gmail.com>
2022-04-05 16:38:58 -0600

    Session2 lab05,06,07

---
+++ 02_JAVA/src/students/mahir/lab07.java
@@ -0,0 +1,41 @@
+package students.mahir;
+
+public class lab07 {
+
+    //       Write a return method that can reverse
+    //       number and return it as int
+    // example =>  123 expected > 321;
+
+    public static void main(String[] args) {
+
+        System.out.println("reverseInt( 54) = " + reverseInt(54)); //expected answer= 45
+        System.out.println("reverseInt(6721) = " + reverseInt(6721)); // expected answer= 1276
+    }
+
+
+    public static int reverseInt(int number){
+
+        int answer=0; // will use to store my answer;
+
+        //    12 !=0
+        //     1 !=0
+        // 0 != 0 false
+        while (number!=0){
+
+            int lastDigit= number % 10; // 3 , 12 %10 == 2; 1 %10 =1;
+
+            //    = 0 *10      +     3
+            //     =  3  * 10 + 2
+            //     = 32 *  10  + 1
+            answer= answer *10 +lastDigit;  // 3 , 32 , 321
+
+            number /=10; //  123 /10= > number= 12 , 12 /10 ==> 1 ,  1 /10 = 0;
+
+
+
+        }
+
+        return answer;
+
+    }
+}
