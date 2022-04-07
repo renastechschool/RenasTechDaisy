a9766e4b2108ae42c51b1f2a470b049d853ed363
zelal <zelalyildizpek@gmail.com>
2022-04-07 11:13:23 -0700

    java labs finished

--- 04_CUCUMBER/src/test/java/students/zelal/lab01.java
+++ 04_CUCUMBER/src/test/java/students/zelal/lab01.java
@@ -1,6 +1,34 @@
-package students.zelal;
-
-public class lab01 {
 
 
-}
+##  - Create feature file steps for below scenarios :
+        ####            1) Verify that "Get Quote" button  is navigating application ?
+        ####            2) Verify that in finance detail you should be able to select only one option.
+        ####            3) Verify that when you complete application you should be able to see "Let us call you" button
+        https://renas-practice.herokuapp.com/home
+
+        Feature: renas-practice INSURANCE functional test
+
+
+        Background: :
+        Given The user wants to go to web site
+        Then The user wants to see Insurance and click
+
+
+        Scenario: Verify that "Get Quote" button  is navigating application
+        When The user wants to see Get Quote and click
+
+        Scenario: Verify that in finance detail should be able to select only one option
+        When The user wants to see Get Quote and click
+        Then The user  should be able to select only one option in finance detail
+
+        Scenario:Verify that when complete application, the user should be able to see "Let us call you" button
+        When The user wants to see Get Quote and click
+        Then The user should be able to select only one option in finance detail and click
+        Then The user should be able to click next button
+        Then
+        Driving Distance
+
+
+
+
+
