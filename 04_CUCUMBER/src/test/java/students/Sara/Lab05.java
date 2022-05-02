081f9d7046bcda38896a69390f807ed926812cb9
sararad12 <sjamalirad@gmail.com>
2022-05-01 21:55:21 -0400

    saraCucumberBDDProj

---
+++ 04_CUCUMBER/src/test/java/students/Sara/Lab05.java
@@ -0,0 +1,22 @@
+package students.Sara;
+
+public class Lab05 {
+}
+/*
+➔ It’s a Cucumber-JUnit framework built by Maven.
+➔ It is written in Java, Hybrid: Data Driven & BDD framework.
+➔ For UI automation testing, we use SELENIUM WebDriver ; for API we useRestAssured , and for Database Testing we use the JDBC libraries.
+➔ Generates json and HTML REPORTS with all the test steps, screenshots and the testmetrics.
+➔ It is structured based on PAGE OBJECT MODEL which makes maintanance very easy, and increases reusibility and redability of my code
+so i have my page classes where I store my elements for each webpage. and theit coresponding merthods
+and uses the SINGLETONwebdriver.
+➔ I use the GIT version control system in my company. I store myautomation framework code in a specific git repository.
+➔ JENKINS for Continuous Integration to run the tests in AWS EC2 Machines.
+➔ Hybrid Framework: Data Driven and Behavior Driven
+➔ Maven: It was a maven project which helped to manage the dependencies, run thetest from the command line, and generate reports.
+➔ Java: I used Java as the programming language - working frontend, backend(api),
+and database I use Java Collection framework to store data and compare.
+➔ Properties File: I used a properties file to store sensitive and reusable data like
+URL(environment), username & password, browser, etc.
+➔ JUnit: I use JUnit testing tool to control flow and assert data.
+ */
---
+++ 04_CUCUMBER/src/test/java/students/Sara/Lab06.java
@@ -0,0 +1,5 @@
+package students.Sara;
+
+public class Lab06 {
+    //https://github.com/sararad12/SaraLab06_cucumber
+}
