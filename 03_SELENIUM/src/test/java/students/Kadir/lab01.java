3bbf86b70e4160cd0f24525f64cc83602ab9d1ec
ogun24 <oguenboy@gmail.com>
2022-05-05 14:57:07 -0600

    fixes

--- 03_SELENIUM/src/test/java/students/Kadir/lab01.java
+++ 03_SELENIUM/src/test/java/students/Kadir/lab01.java
@@ -12,8 +12,8 @@ package students.Kadir;
         #### Refresh
         #### Quit browser*/
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
