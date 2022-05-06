1e6826215dfd4207b70f5e184d686682abbd35f7
yasmin82yasmin <yasmin_82_yasmin@outlook.com>
2022-05-05 23:22:57 -0400

    Yasemin DataBase Labs

---
+++ 05_DATABASE_JDBC/src/test/java/students/yasemin/lab04.java
@@ -0,0 +1,45 @@
+package students.yasemin;
+
+public class lab04 {
+    /*
+    1. find the 3rd maximum salary from the employees table (do
+   not include duplicates)
+   select min(salary) from (select distinct salary from employees
+   order by salary desc) where rownum < 4;
+
+   2. find the 5th maximum salary from the employees table (do
+   not include duplicates)
+   select min(salary) from (select distinct salary from employees
+   order by salary desc) where rownum < 6;
+
+   3. find the 7th maximum salary from the employees table (do
+   not include duplicates)
+   select min(salary) from (select distinct salary from employees
+   order by salary desc) where rownum < 8;
+
+   4. find the 10th maximum salary from the employees table (do
+   not include duplicates)
+   select min(salary) from (select distinct salary from employees
+   order by salary desc) where rownum < 11;
+
+   5. find the 3rd minimum salary from the employees table (do not
+   include duplicates)
+   select max(salary) from (select distinct salary from employees
+   order by salary asc) where rownum < 4;
+
+   6. find the 5th minimum salary from the employees table (do not
+   include duplicates)
+   select max(salary) from (select distinct salary from employees
+   order by salary asc) where rownum < 6;
+
+   7. find the 7th maximum salary from the employees table (do
+   not include duplicates)
+   select max(salary) from (select distinct salary from employees
+   order by salary asc) where rownum < 8;
+
+   8. find the 10th maximum salary from the employees table (do
+   not include duplicates)
+   select max(salary) from (select distinct salary from employees
+   order by salary asc) where rownum < 11;
+     */
+}
